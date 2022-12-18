a7bf72359e4d11ea2960561e4ba9252140c9fea0
igormivanov <igormivanovx@gmail.com>
2022-12-17 23:16:47 -0300

    Salvando dados do formulário no banco de dados

--- src/main/java/com/eventoapp/controllers/EventoController.java
+++ src/main/java/com/eventoapp/controllers/EventoController.java
@@ -1,13 +1,29 @@
 package com.eventoapp.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.eventoapp.models.Evento;
+import com.eventoapp.repository.EventoRepository;
 
 @Controller
 public class EventoController {
-
-	@RequestMapping("/cadastrarEvento") // Indica o endpoint(URL) que deverá ser acessado na requisição 
+	
+	@Autowired // Essa anotação significa que toda vez que precisarmos utilizar essa interface, será criado uma nova instancia automaticamente
+	private EventoRepository eventoRepository;
+	
+	@RequestMapping(value = "/cadastrarEvento", method = RequestMethod.GET) // Indica o endpoint(URL) que deverá ser acessado na requisição 
 	public String form() {
 		return "evento/formEvento"; // Indica o local que está o seu html, geralmente ele está nos templates
+	}
+
+	@RequestMapping(value = "/cadastrarEvento", method = RequestMethod.POST) 
+	public String form(Evento evento) {
+		
+		eventoRepository.save(evento);
+		
+		return "redirect:/cadastrarEvento"; 
 	}
 }
