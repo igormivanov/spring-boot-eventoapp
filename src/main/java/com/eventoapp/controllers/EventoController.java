4265c9023bc14648539dc25eb94cbbd5128379be
igormivanov <igormivanovx@gmail.com>
2022-12-18 00:15:07 -0300

    Realizando buscas no banco de dados MySQL

--- src/main/java/com/eventoapp/controllers/EventoController.java
+++ src/main/java/com/eventoapp/controllers/EventoController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.eventoapp.models.Evento;
 import com.eventoapp.repository.EventoRepository;
@@ -22,8 +23,16 @@ public class EventoController {
 	@RequestMapping(value = "/cadastrarEvento", method = RequestMethod.POST) 
 	public String form(Evento evento) {
 		
-		eventoRepository.save(evento);
+		eventoRepository.save(evento); 
 		
 		return "redirect:/cadastrarEvento"; 
+	}
+	
+	@RequestMapping("/eventos")
+	public ModelAndView listaEventos() {
+		ModelAndView mv = new ModelAndView("index"); // Dentro do () Colocar a url a ser exibida
+		Iterable<Evento> eventos = eventoRepository.findAll();
+		mv.addObject("eventos", eventos);
+		return mv;
 	}
 }
